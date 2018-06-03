9fd46fdfd0785d2baad456623948a6e5bfc26536
vsmithy <videl.smith@gmail.com>
2018-06-03 15:45:58 -0500

    styled calendar portion of rawMarkup.html

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -5,15 +5,15 @@ import React, { Component } from 'react'
 import ClassicContainer from './ClassicContainer'
 import LeaveContainer from './LeaveContainer'
 
-// export default class AppContainer extends Component {
-//   render() {
-//     return (
-//       <div className='appContainer'>
-//         <ClassicContainer />
-//       </div>      
-//     )//return
-//   }//render
-// }//AppContainer
+export default class AppContainer extends Component {
+  render() {
+    return (
+      <div className='appContainer'>
+        <ClassicContainer />
+      </div>      
+    )//return
+  }//render
+}//AppContainer
 
-const AppContainer = () => { return <div className='appContainer'><LeaveContainer /></div>}
-export default AppContainer
+// const AppContainer = () => { return <div className='appContainer'><LeaveContainer /></div>}
+// export default AppContainer
